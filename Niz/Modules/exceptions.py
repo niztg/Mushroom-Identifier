04bd7b05439c67690c90f9672c51b58da4e29a4b
niztg <65248634+niztg@users.noreply.github.com>
2023-02-25 22:09:39 -0500

    dedicated folder

---
+++ Niz/Modules/exceptions.py
@@ -0,0 +1,22 @@
+__all__ = (
+    'InvalidDateOfBirth',
+    'NotOldEnough',
+    'UsernameNotRightLength',
+    "EmailNotProper"
+)
+
+class InvalidDateOfBirth(Exception):
+    def __str__(self):
+        return "Date of birth must be entered in the following format: YYYY-MM-DD"
+
+class NotOldEnough(Exception):
+    def __str__(self):
+        return "You are not old enough!"
+
+class UsernameNotRightLength(Exception):
+    def __str__(self):
+        return "Username and display name must be between 6 and 20 characters!"
+
+class EmailNotProper(Exception):
+    def __str__(self):
+        return "Please enter a valid email"
