108413d2f5535b00069cd2e14294730b48bf5c07
niztg <65248634+niztg@users.noreply.github.com>
2023-02-26 22:05:06 -0500

    hello

---
+++ Accounts/Modules/classes.py
@@ -0,0 +1,41 @@
+"""
+Classes
+"""
+from .exceptions import * 
+from datetime import datetime
+
+__all__ = (
+    'Account'
+)
+
+class Account:
+    """
+    Defines an account   
+    Attributes:
+        name: str = The user's account name
+        display_name[Optional]: str = the user's display name
+        password: str = The user's password
+        email: str = The user's email ID
+        dob: str = The user's date of birth
+    """
+    def __init__(self, name: str, password: str, email: str, dob: datetime, display_name=None):
+        """
+        Initializes the class
+        """
+        self._name = name
+        self._display_name = display_name
+        self._password = password
+        self._email = email
+        self._dob = dob
+
+    @classmethod
+    def from_username_password(cls, username, password):
+        """
+        Initializes class from username and password
+        Args:
+            username: str
+            password: str
+        Returns:
+            Account
+        """
+        ...
---
+++ Accounts/tests.py
@@ -0,0 +1,11 @@
+""""
+Test file!
+"""
+
+import sqlite3
+from datetime import datetime
+
+from Modules.functions import create_account
+from Modules.classes import Account
+
+print(create_account("nizcomix", "idsohuasiu", "hello@worddld.com", "08", "21", "2006", "niz"))
