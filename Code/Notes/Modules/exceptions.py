77b58a4cb836376958ad2019a187aea618b834bb
niztg <65248634+niztg@users.noreply.github.com>
2023-03-03 08:22:33 -0500

    new formatting

--- Code/Notes/Modules/exceptions.py
+++ Code/Notes/Modules/exceptions.py
@@ -7,6 +7,7 @@ class NoteTooLong(Exception):
     def __repr__(self):
         return "Your note is too long! The maximum character limit is 140."
 
+
 class NoteDoesntExist(Exception):
     def __repr__(self):
         return "This note does not exist!"
